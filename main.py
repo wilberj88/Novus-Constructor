cdeb77f9d09f1447d90dd93678dd1a07cbb58479
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-09-28 10:34:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from streamlit_extras.let_it_rain import rain
 # SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
 st.set_page_config(layout="wide", page_title="Novus Constructor", page_icon="🏗️")
 
-st.title('Novus Constructor 🏗️ Automated Cycle')
+st.title('Novus Constructor 🏗️')
+st.header('⚙️ · 🤖 Automated Cycle · 🦾 · 🛠️')
 
 st.write('---')
 
 service = st.selectbox(
         "Which service do you prefer?",
-        ("Licences", "Expert Report"),
+        ("Licences", "Expert Report"),index=None,
+    placeholder="Select an automated service, you just need your documents to charge..."
     )
 
 if service == "Licences":
